b0710b0ef595d5b6bb25820649584c60c7994834
thebigsasha <sashathethird@gmail.com>
2021-07-29 15:16:47 -0400

    Add config for gh pages 🚧

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pathPrefix: "/reponame",
+  pathPrefix: "/sashaphoto-gatsby-template",
   siteMetadata: {
     title: `Sashaphoto Gatsby Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
