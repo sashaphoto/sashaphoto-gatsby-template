358ae8b92ca3603fdf1b0b55200113c5a6fcaf80
TheBigSasha <TheBigSasha@users.noreply.github.com>
2022-04-03 22:17:27 +0000

    deployed via Gatsby Publish Action 🎩 for b9f6f1e677eb94cc99d76934cde1d74c148caf66

---
+++ component---src-pages-page-2-tsx-3075daf314acdd5f11b6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[473],{5267:function(e,t,a){a.r(t);var l=a(7294),n=a(1597),r=a(1944),c=a(8306);t.default=function(){return l.createElement(r.Z,null,l.createElement(c.Z,{title:"Page two"}),l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-tsx-3075daf314acdd5f11b6.js.map
